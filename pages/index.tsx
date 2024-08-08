beec6f2a030b11f92cdb95616c06041001cfaf1e
Aisha <shovon3000g@gmail.com>
2024-08-08 08:13:40 -0700

    added description

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ export default function home() {
     <>
       <Head>
         <title>Aisha Evering | Software Engineer</title>
-        <meta name="description" content="Bleep boop" />
+        <meta
+          name="description"
+          content="Showcasing a blend of practical expertise and innovative models, my portfolio highlights my journey through data science and machine learning with interactive apps and real-world solutions."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
